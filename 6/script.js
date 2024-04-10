af8b5f30ebd9bd22a9d7d6fce71c32131d430d3f
michaeland974 <micanderson29@gmail.com>
2024-04-10 11:55:55 -0500

    refactor: cleanup

--- 6/script.js
+++ 6/script.js
@@ -1,33 +1,25 @@
 (() => {
-  
-const stylesheets = document.styleSheets;
-const ss = stylesheets[0];
-const rules = ss.cssRules;
+  const stylesheets = document.styleSheets;
+  const ss = stylesheets[0];
+  const rules = ss.cssRules;
 
-for(let i = 0; i < rules.length; i++){
-   const rule = rules[i];
-   const hasMediaQuery = (rule.cssText).startsWith('@media');
-   
-   if(hasMediaQuery){
-    const ruleList = rule.cssRules;
-    const selector = ruleList[0].selectorText;
-    const mediaQuery = ruleList[0].parentRule.media.mediaText;
+  for(let i = 0; i < rules.length; i++){
+    const rule = rules[i];
+    const hasMediaQuery = (rule.cssText).startsWith('@media');
     
-    // if(ruleList.length > 1){
-    //   console.log(ruleList);
-    // }
-
-    if(mediaQuery.includes('width: 991px')){
-      const property = 'margin', value = '0px';
-      const fullRuleSet = `@media ${mediaQuery} {
-        ${selector} { ${property}: ${value}; } 
-      }`;
-      console.log(fullRuleSet);
-      ss.deleteRule(i);
+    if(hasMediaQuery){
+      const ruleList = rule.cssRules;
+      const selector = ruleList[0].selectorText;
+      const mediaQuery = ruleList[0].parentRule.media.mediaText;
+      
+      if(mediaQuery.includes('width: 991px')){
+        const property = 'margin', value = '0px';
+        const fullRuleSet = `@media ${mediaQuery} {
+          ${selector} { ${property}: ${value}; } 
+        }`;
+        ss.deleteRule(i);
+      }
     }
-
   }
-}
-
 })();
 
