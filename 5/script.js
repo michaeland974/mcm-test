3846aa2c76ebf14b2a467a823ced10c932d5d2f6
michaeland974 <micanderson29@gmail.com>
2024-04-16 00:04:47 -0500

    fix: proper render on initial load

--- 5/script.js
+++ 5/script.js
@@ -1,33 +1,34 @@
-const selector = 'div > .frvtbum > img';
-const { clone, width, height } = getImageElement(selector);
+window.onload = (event) => {
+  const parent = document.querySelector('div');
+  const selector = 'div > .frvtbum > img';
+  const { clone, width, height } = getImageElement(selector);
 
-const canvas = createElement('canvas', 'canvas-drop', width, height);
-const ctx = canvas.getContext('2d');
-const position = (width / 2) - 10;
+  const canvas = createElement('canvas', 'canvas-drop', width, height, parent);
+  const ctx = canvas.getContext('2d');
+  const position = (width / 2) - 10;
 
-ctx.drawImage(clone, 0, 0, width, height);
-ctx.clearRect(position, position, width/2, height/3);
-ctx.save();
+  ctx.drawImage(clone, 0, 0, width, height);
+  ctx.clearRect(position, position, width/2, height);
+  ctx.save();
 
-function createElement(tag, id, width, height){
-  const element = document.createElement(tag);
-  if(document.getElementById(id) === null){
-    element.id = id;
-  }
-  if(tag === 'canvas'){
-    element.width = width;
-    element.height = height;
-  }
-  document.body.appendChild(element);
-  return element;
-};
+  function createElement(tag, id, width, height, parent){
+    const element = document.createElement(tag);
+    if(document.getElementById(id) === null){
+      element.id = id;
+    }
+    if(tag === 'canvas'){
+      element.width = width;
+      element.height = height;
+    }
+    parent.appendChild(element);
+    return element;
+  };
 
-function getImageElement(selector){
-  const el = document.querySelector(selector);
-  const clone = el.cloneNode(true);
-  const {width, height} = clone;
-  return {clone, width, height};
-};
-
-
+  function getImageElement(selector){
+    const el = document.querySelector(selector);
+    const clone = el.cloneNode(true);
+    const {width, height} = clone;
+    return {clone, width, height};
+  };
+}
 
