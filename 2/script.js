0c370cd20b39ded6f3cca197a8af8de77ca3ab59
michaeland974 <micanderson29@gmail.com>
2024-04-16 04:41:40 -0500

    fix: script updated to toggle nav element attribute

--- 2/script.js
+++ 2/script.js
@@ -1,7 +1,9 @@
 const button = document.querySelector('#menu-toggle');
+const menu = document.querySelector('.menu');
 
 button.addEventListener('click', () => {
   const condition = button.getAttribute('aria-expanded');
-  const newAtt = condition === 'false' ? 'true' : 'false'
+  const newAtt = (condition === 'false') ? 'true' : 'false';
   button.setAttribute('aria-expanded', newAtt);
+  menu.setAttribute('aria-expanded', newAtt);
 });
